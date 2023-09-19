108d03a853e04c668fe988614b24228df40f06dc
Dharmendra Sikarwar <dsikarwar@victoria.com>
2023-09-19 23:45:05 +0530

    fixed custom raised component

--- components/custom/Raised.tsx
+++ components/custom/Raised.tsx
@@ -1,7 +1,7 @@
 import type { FC } from "react";
 
 const Raised: FC = () => {
-  return <div className="bg-white shadow-raised-box">{children}</div>;
+  return <div className="bg-white shadow-raised-box">tt</div>;
 };
 
 export default Raised;
