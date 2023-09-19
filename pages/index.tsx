c811a70e3113b18e8bdde4b0c888c07516231f3d
Dharmendra Sikarwar <dsikarwar@victoria.com>
2023-09-19 23:37:25 +0530

    fixed aside

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
+import Aside from "@components/Aside";
 import type { FC } from "react";
 import { Button } from "@components/ui/button";
 
@@ -16,6 +17,7 @@ const Home: FC = () => {
         <div className="flex-1">
           <div className="border-b">
             <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
+              <Aside />
               Get started by editing the <code>pages/index.js</code>
               <Button className="bg-white">Button</Button>
             </div>
